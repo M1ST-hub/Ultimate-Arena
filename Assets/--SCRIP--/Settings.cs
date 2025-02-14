62811bf4dbe0ec358f40f177860700805d8dc7b3
M1ST-hub <143425337+M1ST-hub@users.noreply.github.com>
2025-02-14 10:49:10 -0600

    menu tings

---
+++ Assets/--SCRIP--/Settings.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    private Slider slider;
+    private TextMeshProUGUI value;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        value = GetComponentInChildren<TextMeshProUGUI>();
+
+        if (slider != null && value != null)
+        {
+            // Set the initial text value based on the current slider value
+            UpdateText(slider.value);
+
+            // Add listener to update the text when the slider value changes
+            slider.onValueChanged.AddListener(UpdateText);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateText(float value)
+    {
+        // Update the displayText string based on the slider value
+        this.value.text = value.ToString("F0");
+    }
+}
