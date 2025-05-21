9f73cd026215369aa17ff46e88f5d0a55a44670e
M1ST-hub <143425337+M1ST-hub@users.noreply.github.com>
2025-05-21 12:08:19 -0500

    lessP

--- Assets/--SCRIP--/PlayerController.cs
+++ Assets/--SCRIP--/PlayerController.cs
@@ -200,17 +200,17 @@ public class PlayerController : NetworkBehaviour
                     taggedTime += Time.deltaTime;
                     netTaggedTime.Value = taggedTime;
 
-                    int xpGain = Mathf.RoundToInt((taggedTime * (5f / 6f)) * taggerExp * Time.deltaTime);
+                    int xpGain = Mathf.RoundToInt((taggedTime * (0.6f / 6f)) * taggerExp * Time.deltaTime);
                     Debug.Log($"Tagged XP {xpGain}");
                     currentXp.Value += xpGain;
                 }
                 else
                 {
-                    untaggedMult += Time.deltaTime;
+                    untaggedMult += (Time.deltaTime / 1.3f);
                     untaggedTime += Time.deltaTime;
                     netUntaggedTime.Value = untaggedTime;
                     
-                    multiplier = Mathf.Min(1f + (untaggedMult / 10f), maxMultiplier);
+                    multiplier = Mathf.Min(0.3f + (untaggedMult / 13f), maxMultiplier);
                     int xpGain = Mathf.RoundToInt(surviveExp * multiplier * Time.deltaTime);
                     Debug.Log($"Untagged XP {xpGain}");
                     currentXp.Value += xpGain;
